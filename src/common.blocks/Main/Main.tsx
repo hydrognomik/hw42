b1ad7db1fe192416075556c0cb2ca40a9f9ed3d2
Aleksey Petrov <hydrognomik@gmail.com>
2018-11-14 11:52:19 +0300

    feat(commmon): добавил инжекшн компонента контент

--- src/common.blocks/Main/Main.tsx
+++ src/common.blocks/Main/Main.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { cn } from '@bem-react/classname';
+import { RegistryConsumer } from '@bem-react/di';
 
-import { Content } from '../Content/Content';
+import { Content as CommonContent } from '../Content/Content';
 
 import './Main.css';
 
-const main = cn('Main');
+const cnApp = cn('App');
+const cnConten = cn('Conten');
+const cnMain = cn('Main');
 
 export const Main = () => (
-  <main className={main()}>
-    <div className={main('Header')}>Лента событий</div>
-    <Content />
-  </main>
+  <RegistryConsumer>
+    {registries => {
+
+      const registry = registries[cnApp()];
+      let Content;
+
+      try {
+        Content = registry.get(cnConten());
+      } catch (err) {
+        Content = CommonContent;
+      }
+
+      return (
+        <main className={cnMain()}>
+          <div className={cnMain('Header')}>Лента событий</div>
+          <Content />
+        </main>
+      );
+    }}
+  </RegistryConsumer>
 );
