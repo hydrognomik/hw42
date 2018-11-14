2008a2db28a6ecb7bba12883ccb91d679a0b40c0
Aleksey Petrov <hydrognomik@gmail.com>
2018-11-14 12:18:31 +0300

    bug: вебпак собирает все стили, а не только нужные

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { isMobile } from 'react-device-detect';
 
 import AppDesktop from './desktop.blocks/App';
+import AppTouch from './touch.blocks/App';
 
 import './index.css';
 
 ReactDOM.render(
-  <AppDesktop />,
+  isMobile ? <AppTouch /> : <AppDesktop />,
   document.getElementById('root'),
 );
